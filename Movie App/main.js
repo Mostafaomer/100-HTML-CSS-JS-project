7cdd4c398c78f234e20123f3ab2310b3734d7750
Mostafaomer <87620409+Mostafaomer@users.noreply.github.com>
2023-04-19 16:54:05 +0200

    Update main.js

--- Movie App/main.js
+++ Movie App/main.js
@@ -1,6 +1,6 @@
 const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const main=document.querySelector("main")
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=2`;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=3`;
 async function getFilms() {
     let res=await fetch(API_URL)
     let data=await res.json()
