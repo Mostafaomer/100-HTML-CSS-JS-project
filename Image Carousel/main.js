b47536f94c077b920262f7c01e6f730dca312c54
Mostafaomer <87620409+Mostafaomer@users.noreply.github.com>
2023-04-20 02:21:37 +0200

    Update main.js

--- Image Carousel/main.js
+++ Image Carousel/main.js
@@ -1,16 +1,24 @@
-let next=document.getElementById("next")
-let current =0
+// let next=document.getElementById("next")
+let current =1
 let imgs=document.querySelector(".imgs")
-next.addEventListener('click' , () => {
-    current++
-    if(current==3)
+// next.addEventListener('click' , () => {
+//     imgs.style.transform = `translateX(${-current * 500}px)`
+//     current++
+//     if(current==3)
+//         current=0
+// })
+// prev.addEventListener('click' , () => {
+//     if(current<=1)
+//         current=2
+//     else{
+//         imgs.style.transform = `translateX(${-current * 500}px)`
+//         current--
+//     }
+// })
+setInterval(()=> {
+    current++;
+    if(current==3) 
         current=0
-    imgs.style.transform = `translateX(${-current * 500}px)`
-   
-})
-prev.addEventListener('click' , () => {
-    current--
-    if(current==-1)
-        current=2
-    imgs.style.transform = `translateX(${-current * 500}px)`
-})
+    imgs.style.transform = `translateX(${-current * 800}px)`
+
+},2000)
